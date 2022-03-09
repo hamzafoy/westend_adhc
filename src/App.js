204a70abf80fece30b45a49138cc072afba1f31a
hamzafoy <hamzaoflouisville@gmail.com>
2022-03-08 20:39:33 -0500

    replacing boilerplate text.

--- src/App.js
+++ src/App.js
@@ -7,16 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>This is a work in progress for Mary Stanton &amp; Abu Bakr for West End ADHC</code>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
