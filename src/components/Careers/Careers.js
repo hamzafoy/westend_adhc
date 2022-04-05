1ed077e3c2b8282c4ece5b8bd3ed1a255ab82844
hamzafoy <hamzaoflouisville@gmail.com>
2022-04-05 16:52:30 -0400

    build out the Careers page with cards for each position.

--- src/components/Careers/Careers.js
+++ src/components/Careers/Careers.js
@@ -107,6 +107,90 @@ class Careers extends React.Component {
                             Careers at West End ADHC
                         </h1>
 
+                        <p className="careers-content_info">
+                        We offer competitive wages, flexible schedules, eligibility for employee 
+                        benefits and treats you with the utmost respect. We're hiring passionate 
+                        and reliable caregivers who have the desire and skills to assist and support 
+                        disabled individuals and seniors with the care they deserve. You may find 
+                        current job openings below.
+                        </p>
+
+                        <div className="careers-content_cards">
+
+                            <article className="careers-content__card">
+
+                                <h1>
+                                    CNA
+                                </h1>
+
+                                <svg width="54" height="54" stroke-width="1" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
+                                    <path d="M4.271 18.3457C4.271 18.3457 6.50002 15.5 12 15.5C17.5 15.5 19.7291 18.3457 19.7291 18.3457" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
+                                    <path d="M12 12C13.6569 12 15 10.6569 15 9C15 7.34315 13.6569 6 12 6C10.3431 6 9 7.34315 9 9C9 10.6569 10.3431 12 12 12Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
+                                </svg>
+
+                                <p>
+                                    This role is for CNA
+                                </p>
+
+                            </article>
+
+                            <article className="careers-content__card">
+
+                                <h1>
+                                    Medical Van Driver
+                                </h1>
+
+                                <svg width="54" height="54" stroke-width="1" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
+                                    <path d="M4.271 18.3457C4.271 18.3457 6.50002 15.5 12 15.5C17.5 15.5 19.7291 18.3457 19.7291 18.3457" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
+                                    <path d="M12 12C13.6569 12 15 10.6569 15 9C15 7.34315 13.6569 6 12 6C10.3431 6 9 7.34315 9 9C9 10.6569 10.3431 12 12 12Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
+                                </svg>
+
+                                <p>
+                                    This role is for MVD
+                                </p>
+
+                            </article>
+
+                            <article className="careers-content__card">
+
+                                <h1>
+                                    Caregiver
+                                </h1>
+
+                                <svg width="54" height="54" stroke-width="1" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
+                                    <path d="M4.271 18.3457C4.271 18.3457 6.50002 15.5 12 15.5C17.5 15.5 19.7291 18.3457 19.7291 18.3457" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
+                                    <path d="M12 12C13.6569 12 15 10.6569 15 9C15 7.34315 13.6569 6 12 6C10.3431 6 9 7.34315 9 9C9 10.6569 10.3431 12 12 12Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
+                                </svg>
+
+                                <p>
+                                    This role is for Caregiver
+                                </p>
+
+                            </article>
+
+                            <article className="careers-content__card">
+
+                                <h1>
+                                    Case Manager
+                                </h1>
+
+                                <svg width="54" height="54" stroke-width="1" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
+                                    <path d="M4.271 18.3457C4.271 18.3457 6.50002 15.5 12 15.5C17.5 15.5 19.7291 18.3457 19.7291 18.3457" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
+                                    <path d="M12 12C13.6569 12 15 10.6569 15 9C15 7.34315 13.6569 6 12 6C10.3431 6 9 7.34315 9 9C9 10.6569 10.3431 12 12 12Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
+                                </svg>
+
+                                <p>
+                                    This role is for Case Mgr
+                                </p>
+
+                            </article>
+
+                        </div>
+
                     </main>
 
                     <footer className="standard-footer">
